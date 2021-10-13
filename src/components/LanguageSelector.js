64fbf596343a383768dd9fea0177cfb7593d5946
koechh <koechh788@gmail.com>
2021-10-13 08:01:11 +0300

    Used this.context in LanguageSelector to access data in LanguagStore

--- src/components/LanguageSelector.js
+++ src/components/LanguageSelector.js
@@ -2,17 +2,16 @@ import React, { Component } from "react";
 import LanguageContext from "../contexts/LanguageContext";
 
 class LanguageSelector extends Component {
+  static contextType = LanguageContext;
+
   render() {
+    const { onLanguageChange } = this.context;
     return (
-      <LanguageContext.Consumer>
-        {( { language, onLanguageChange } ) =>
-          <div>
-            Select a language:
-            <i className="flag us" onClick={() => onLanguageChange('english')} />
-            <i className="flag nl" onClick={() => onLanguageChange('dutch')} />
-          </div>
-        }
-      </LanguageContext.Consumer>
+      <div>
+        Select a language:
+        <i className="flag us" onClick={() => onLanguageChange('english')}/>
+        <i className="flag nl" onClick={() => onLanguageChange('dutch')}/>
+      </div>
     );
   }
 }
