93caee0862b7802d715aef210cabf103ed2cf63d
koechh <koechh788@gmail.com>
2021-10-13 06:58:33 +0300

    refractor and cleanup on render function

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,17 +4,20 @@ import ColorContext from "../contexts/ColorContext";
 
 class Button extends Component {
 
+  renderButton( color ) {
+    return (
+      <button className={`ui button ${color}`}>
+        <LanguageContext.Consumer>
+          {value => value === 'english' ? 'Submit' : 'Voorleggen'}
+        </LanguageContext.Consumer>
+      </button>
+    );
+  }
+
   render() {
     return (
       <ColorContext.Consumer>
-        {( color ) => (
-          <button className={`ui button ${color}`}>
-            <LanguageContext.Consumer>
-              {value => value === 'english' ? 'Submit' : 'Voorleggen'}
-            </LanguageContext.Consumer>
-          </button>
-        )
-        }
+        {color => ( this.renderButton(color) )}
       </ColorContext.Consumer>
     );
   }
